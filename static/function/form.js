dfa2ce23951a9579d4cb3286bb3a504d6e6d4cf2
ange <gmail.com>
2024-06-22 00:13:12 +0800

    Modified token auth fetch

--- static/function/form.js
+++ static/function/form.js
@@ -111,9 +111,33 @@ function logout() {
   window.location.href = "http://127.0.0.1:8000/";
 }
 
-window.addEventListener("load", function () {
-  const token = localStorage.getItem("token");
-  if (token) {
-    showLogoutButton();
+const token = localStorage.getItem("token");
+fetchUser(token);
+
+async function fetchUser(token) {
+  try {
+    if (!token) {
+      return;
+    }
+    const response = await fetch("http://127.0.0.1:8000/api/user/auth", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data.data);
+    if (data.data !== null) {
+      showLogoutButton();
+    } else {
+      logout();
+    }
+  } catch (error) {
+    console.error("Error:", error);
   }
-});
+}
